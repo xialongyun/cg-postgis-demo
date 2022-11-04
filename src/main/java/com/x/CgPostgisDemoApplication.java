5e473f592d628b12482b0eb13520efae68ae1ef6
xialongun <xia_longyun@163.com>
2022-11-05 00:03:12 +0800

    project init

---
+++ src/main/java/com/x/CgPostgisDemoApplication.java
@@ -0,0 +1,13 @@
+package com.x;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgPostgisDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CgPostgisDemoApplication.class, args);
+    }
+
+}
